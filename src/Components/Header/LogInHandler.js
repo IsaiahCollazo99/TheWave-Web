f9c8f94c98b952ac14e92c17cdb7fbc41bdd88ac
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2021-01-17 20:37:16 -0500

    setup Firebase

---
+++ src/Components/Header/LogInHandler.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogInHandler = () => {
+    return (
+        <>
+        <button className="logInHandler">Log In</button>
+        </>
+    )
+}
+
+export default LogInHandler;
