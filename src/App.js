3174fd2a466f6c90a7c062c6d3408604146a25a0
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2021-01-17 22:59:07 -0500

    header css, edit Log In Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Route } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
         <Header />
+
+        <main className="App-Main">
+          <Route path="/login">
+            <Login />
+          </Route>
+        </main>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+
+    return (
+        <section className="logInContainer">
+            <h2>Log In</h2>
+        </section>
+    )
+}
+
+export default Login;
