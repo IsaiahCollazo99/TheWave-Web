fe6bd4297d6177b2673a56cf3c6a37a776ed330d
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2021-01-17 19:15:29 -0500

    init React App, create Header Component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="App-header">
+          <section className="App-hl">
+            <h1>The Wave</h1>
+          </section>
+
+          <section className="App-hr">
+            
+          </section>
+        </header>
+    )
+}
+
+export default Header;
